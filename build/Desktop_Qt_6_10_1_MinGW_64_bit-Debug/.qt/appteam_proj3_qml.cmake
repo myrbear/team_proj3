0247995e84b54e7161439d0c9108a9412808eeef
myrbear <mzlafty@gmail.com>
2026-02-24 12:56:13 -0500

    stuff

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appteam_proj3_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appteam_proj3")
+set(working_dir "C:/Users/mzlaf/Documents/proj/team_proj3")
+set(src_and_dest_list
+    "C:/Users/mzlaf/Documents/proj/team_proj3/Main.qml"
+    "C:/Users/mzlaf/Documents/proj/team_proj3/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/team_proj3/Main.qml"
+
+)
+set(timestamp_file "C:/Users/mzlaf/Documents/proj/team_proj3/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appteam_proj3_qml.txt")
