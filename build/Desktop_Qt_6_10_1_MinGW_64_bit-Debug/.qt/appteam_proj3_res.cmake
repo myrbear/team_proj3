1d9de1b0bbc2b1eb154873643a2e3b0c2a1aca63
zpica20 <zpica2004@gmail.com>
2026-02-24 13:24:14 -0500

    first commit

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appteam_proj3_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appteam_proj3")
-set(working_dir "C:/Users/mzlaf/Documents/proj/team_proj3")
-set(src_and_dest_list
-
-)
-set(timestamp_file "C:/Users/mzlaf/Documents/proj/team_proj3/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/appteam_proj3_res.txt")
--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include "mainwindow.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("team_proj3", "Main");
-
-    return app.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
